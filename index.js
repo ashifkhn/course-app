463153ca9391dfa700f566a63e2c0455fed431f0
Ashif Khan <ashif@Ashifs-MacBook-Air.local>
2023-09-27 16:35:26 +0530

    Signin basic api

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import e from "express"
 import express from "express"
 const app = express()
 const port = 3000
@@ -8,7 +9,7 @@ app.get('/', (req, res) => {
     res.status(200).send(ADMINS)
 })
 
-app.post('/', (req, res) => {   
+app.post('/admin/signup', (req, res) => {   
     const admin = req.body
     const alreadyExist=ADMINS.find(user=>user.username===admin.username)
     if(alreadyExist){
@@ -18,6 +19,19 @@ app.post('/', (req, res) => {
     ADMINS.push(admin)
     res.status(201).json({admin,message:"Admin created successfully"})
     }
+})
+
+app.post('/admin/signin', (req, res) => {
+
+    const admin = req.body
+    const ifUserExists = ADMINS.find(user=>user.username===admin.username && user.password===admin.password)
+    if (ifUserExists){
+            res.status(200).json({admin,message:"Login Successful"})
+    }
+    else{
+        res.status(200).json({admin,message:"User does not exist"})
+    }
+
 })
 
 app.listen(port, () => {
